c1dc8c324871356fc8a657838dc8e82203af4d49
Lea Tortay <leatortay@gmail.com>
2020-03-19 14:45:15 +0100

    header update

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -3,20 +3,12 @@ module.exports = {
     {
       label: "Accueil",
       url: "/fr/"
-    },
-    {
-      label: "Blog",
-      url: "/fr/blog/"
     }
   ],
   en: [
     {
       label: "Home",
       url: "/en/"
-    },
-    {
-      label: "Blog",
-      url: "/en/blog/"
     }
   ]
 };
